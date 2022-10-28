90730d66586d8f47aea781c9430eaeb9e22d3e83
AbdallaFathy1121 <abdallafathy1023@gmail.com>
2022-10-28 23:13:29 +0200

    Update_ExamCollectionsController

--- E-Exam/Areas/Admin/Controllers/ExamCollectionsController.cs
+++ E-Exam/Areas/Admin/Controllers/ExamCollectionsController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using E_Exam.Core;
+using E_Exam.Core.Models;
 using E_Exam.Core.ViewModels;
 using E_Exam.Utility.Consts;
 using Microsoft.AspNetCore.Authorization;
@@ -27,6 +28,7 @@ namespace E_Exam.Areas.Admin.Controllers
             int subjectId = Convert.ToInt32(split[1]);
 
             ViewBag.SubjectId = subjectId;
+            ViewBag.ExamId = examId;
 
             var result = await _unitOfWork.TbExamCollections.GetWhereAsync(a=>a.ExamId == examId && a.SubjectId == subjectId, new[] {"Exam", "ModelType", "Chapter"});
             
@@ -35,6 +37,99 @@ namespace E_Exam.Areas.Admin.Controllers
 
             return View(model);
         }
+
+        public async Task<IActionResult> Add(string id)
+        {
+            var split = id.Split("--");
+            int examId = Convert.ToInt32(split[0]);
+            int subjectId = Convert.ToInt32(split[1]);
+
+            ViewBag.SubjectId = subjectId;
+            ViewBag.ExamId = examId;
+            ViewBag.Chapters = await _unitOfWork.TbChapters.GetAllAsync();
+            ViewBag.ModelTypes = await _unitOfWork.TbModelTypes.GetAllAsync();
+
+            return View("Edit");
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            var result = await _unitOfWork.TbExamCollections
+                .GetFirstOrDefaultAsync(x => x.Id == id);
+
+            ViewBag.SubjectId = result.SubjectId;
+            ViewBag.ExamId = result.ExamId;
+            ViewBag.Chapters = await _unitOfWork.TbChapters.GetAllAsync();
+            ViewBag.ModelTypes = await _unitOfWork.TbModelTypes.GetAllAsync();
+
+            if (result is not null)
+            {
+                ExamCollectionVM model = new()
+                {
+                    Id = result.Id,
+                    NumberOfQuestions = result.NumberOfQuestions,
+                    ChapterId = result.ChapterId,
+                    ModelTypeId = result.ModelTypeId,
+                };
+
+                return View(model);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(ExamCollectionVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.Id == 0)
+                {
+                    TbExamCollection result = new()
+                    {
+                        SubjectId = model.SubjectId,
+                        ExamId = model.ExamId,
+                        NumberOfQuestions = model.NumberOfQuestions,
+                        ModelTypeId = model.ModelTypeId,
+                        ChapterId=model.ChapterId
+                    };
+
+                    await _unitOfWork.TbExamCollections.AddAsync(result);
+                    _unitOfWork.Complete();
+
+                    TempData["Success"] = "Add Exam Collection Successfully!";
+                }
+                else
+                {
+                    TbExamCollection result = new()
+                    {
+                        Id = model.Id,
+                        SubjectId = model.SubjectId,
+                        ExamId = model.ExamId,
+                        NumberOfQuestions = model.NumberOfQuestions,
+                        ModelTypeId = model.ModelTypeId,
+                        ChapterId = model.ChapterId
+                    };
+
+                    _unitOfWork.TbExamCollections.Update(result);
+                    _unitOfWork.Complete();
+
+                    TempData["Success"] = "Update Exam Collection Successfully!";
+                }
+
+                return Redirect("/Admin/ExamCollections?id=" + model.ExamId+"--"+model.SubjectId);
+            }
+
+            ViewBag.SubjectId = model.SubjectId;
+            ViewBag.ExamId = model.ExamId;
+            ViewBag.Chapters = await _unitOfWork.TbChapters.GetAllAsync();
+            ViewBag.ModelTypes = await _unitOfWork.TbModelTypes.GetAllAsync();
+
+            return View("Edit", model);
+        }
+
+
+
 
 
 
